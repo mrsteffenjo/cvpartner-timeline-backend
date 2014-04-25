de6951868e07177ae589dbc6c1bd1fce1a257277
Steffen Jørgensen <mrsteffenjo@gmail.com>
2014-04-25 15:57:51 +0200

    Initial maven project

---
+++ cvpartner-timeline-backend/src/main/java/no/webstep/fagweekend/App.java
@@ -0,0 +1,13 @@
+package no.webstep.fagweekend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
